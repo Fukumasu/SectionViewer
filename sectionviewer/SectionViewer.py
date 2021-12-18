4abf4ce213ff0d1aa3d5aa2412e79bc2c5305cb6
Fukumasu <32291156+Fukumasu@users.noreply.github.com>
2021-12-18 20:58:30 +0900

    Update SectionViewer.py

--- sectionviewer/SectionViewer.py
+++ sectionviewer/SectionViewer.py
@@ -105,7 +105,7 @@ def main(*arg):
     app.mainloop()
     
 def launch(path=''):
-    subprocess.run('sectionviewer {0}'.format(path), shell=True)
+    subprocess.run(['sectionviewer {0}'.format(path)], shell=True)
     
 if __name__ == "__main__":
     launch()
