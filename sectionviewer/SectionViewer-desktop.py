ba82b7baf5cb20a093e976900277a9aeda00a674
Fukumasu <32291156+Fukumasu@users.noreply.github.com>
2021-12-30 17:57:35 +0900

    Add files via upload

---
+++ sectionviewer/SectionViewer-desktop.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 30 17:53:00 2021
+
+@author: kazuu
+"""
+import os
+import subprocess
+
+eDir = os.path.dirname(os.path.abspath(__file__))
+eDir = os.path.join(eDir, "SectionViewer-install.exe")
+
+subprocess.Popen(eDir, shell=True)
