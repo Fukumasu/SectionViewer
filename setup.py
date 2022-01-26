b4f03f1222ec184baa10b817edb53199d6c3c519
Fukumasu <32291156+Fukumasu@users.noreply.github.com>
2022-01-26 17:44:26 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,21 @@ from os.path import basename, splitext
 import platform
 from setuptools import setup, Extension, find_packages
 
+try:
+    from Cython.Distutils import build_ext
+    USE_CYTHON = True
+   except ImportError:
+    USE_CYTHON = False
+if USE_CYTHON:
+    ext = '.pyx'
+    cmdclass = {'build_ext': build_ext}
+else:
+    ext = '.c'
+    cmdclass = {}
+    
+ext_modules = [Extension('sectionviewer.utils', 
+                         sources=['sectionviewer/utils' + ext])]
+
 def _requires_from_file(filename):
     return open(filename).read().splitlines()
 
@@ -12,6 +27,8 @@ if pf == "Windows":
         name = 'sectionviewer',
         version = '1.0.0',
         packages=['sectionviewer'],
+        ext_modules=ext_modules,
+        cmdclass=cmdclass,
         install_requires=_requires_from_file('requirements.txt'),
         py_modules=[splitext(basename(path))[0] for path in glob('sectionviewer/*.py')],
         package_data={'': ['*.exe', '*.dll', 'img/resources.zip', 'img/SectionViewer.ico', 'SectionViewer/execute.py']},
