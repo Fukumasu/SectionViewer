b0411dc72cf6764085da572130cff2c6075bdce5
Fukumasu <32291156+Fukumasu@users.noreply.github.com>
2022-01-31 15:52:16 +0900

    Update setup_cython.py

--- sectionviewer/setup_cython.py
+++ sectionviewer/setup_cython.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Nov  4 12:01:39 2020
-
-@author: kazuu
-"""
 from distutils.core import setup, Extension
 from Cython.Build import cythonize
 from numpy import get_include
