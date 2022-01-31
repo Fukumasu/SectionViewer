324f9fbf64855e53452e164efab605399966a7f7
Fukumasu <32291156+Fukumasu@users.noreply.github.com>
2022-01-31 15:52:54 +0900

    Delete setup_cython.py

--- sectionviewer/setup_cython.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup, Extension
-from Cython.Build import cythonize
-from numpy import get_include
-
-ext = Extension("utils", sources=["utils.pyx"], extra_compile_args=["/openmp"],
-                include_dirs=[".", get_include()])
-setup(name="utils", ext_modules=cythonize([ext], compiler_directives={"language_level": "3"}))
