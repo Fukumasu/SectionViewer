d30f385328f6c3a265f7726a5e9683f6bedcb301
Kazushi Fukumasu <32291156+Fukumasu@users.noreply.github.com>
2022-02-02 14:29:37 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,9 +5,8 @@ from setuptools import setup, Extension, find_packages
 
 version = '1.0.0'
 
-info = \
+info = 'version = ' + version + \
 '''
-version = '{version}'
 author = 'Kazushi Fukumasu'
 url = 'https://github.com/Fukumasu/SectionViewer'
 upgrade_code = '{6bd9a5e4-428c-4053-8956-9c452ebeefcf}'
