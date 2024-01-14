cf4fdad3eeb2526bf10e3ae7b9973c6547343380
Kazushi Fukumasu <32291156+Fukumasu@users.noreply.github.com>
2024-01-13 22:37:00 -0600

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from glob import glob
 from os.path import basename, splitext
 import platform
-from setuptools import setup, Extension, find_packages
+from setuptools import setup, Extension
 
 def build_ext(*args, **kwargs):
     from Cython.Distutils import build_ext as build_ext_cy
@@ -18,7 +18,7 @@ install_requires = open('requirements.txt').read().splitlines()
 pf = platform.system()
 if pf == 'Windows':
     omp = '/openmp'
-    pd = ['*.txt', '*.pyx', '*.cmd', 'img/resources.png', 'img/icon.ico']
+    pd = ['*.txt', '*.pyx', 'img/resources.png', 'img/icon.ico']
 elif pf == 'Darwin':
     omp = ''
     pd = ['*.txt', '*.pyx', 'img/resources.png']
@@ -27,9 +27,9 @@ elif pf == 'Linux':
     pd = ['*.txt', '*.pyx', 'img/resources.png']
 
 ext_modules = [Extension('sectionviewer.utils', 
-                         sources=['sectionviewer/utils.pyx'],
-                         extra_compile_args=[omp],
-                         extra_link_args=[omp]
+                         sources = ['sectionviewer/utils.pyx'],
+                         extra_compile_args = [omp],
+                         extra_link_args = [omp]
                         )
               ]
 cmdclass = {'build_ext': build_ext}
@@ -41,17 +41,19 @@ open('sectionviewer/info.py', 'w').write(info)
 setup(
     name = 'sectionviewer',
     version = version,
-    packages=['sectionviewer', 'sectionviewer.img'],
-    ext_modules=ext_modules,
+    packages = ['sectionviewer', 'sectionviewer.gui', 
+                'sectionviewer.img', 'sectionviewer.temp'],
+    ext_modules = ext_modules,
     cmdclass=cmdclass,
-    install_requires=install_requires,
-    py_modules=[splitext(basename(path))[0] for path in glob('sectionviewer/*.py')],
-    package_data={'': pd},
-    include_package_data=True,
-    setup_requires=['numpy', 'cython'],
+    install_requires = install_requires,
+    py_modules=[splitext(basename(path))[0]
+                for path in glob('sectionviewer/*.py') + glob('sectionviewer/gui/*.py')],
+    package_data = {'': pd},
+    include_package_data = True,
+    setup_requires = ['numpy', 'cython'],
     entry_points = {
         'console_scripts': [
-            'sectionviewer = sectionviewer.main:console_command'
+            'sectionviewer = sectionviewer.main:main'
         ]
     }
 )
