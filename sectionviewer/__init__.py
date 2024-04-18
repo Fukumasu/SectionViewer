96d38bd359c525e6dc8e4136b82945e98b60dae2
Kazushi Fukumasu <32291156+Fukumasu@users.noreply.github.com>
2024-04-18 11:57:49 -0500

    Update __init__.py

--- sectionviewer/__init__.py
+++ sectionviewer/__init__.py
@@ -1,9 +1,5 @@
 from .core import load
-from .tools import launch, check_version
-from .info import version, url
+from .tools import launch
+from .info import version
 
 __version__ = version
-
-v = check_version()
-if v:
-    print('Version {0} is now available! Please check <{1}>.'.format(v, url))
